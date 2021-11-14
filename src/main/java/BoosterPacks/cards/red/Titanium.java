b685cca7388db3b66ecb6bd3d3837006c3988a3b
H2WO4 <arthurlamidel@gmail.com>
2021-11-14 19:14:56 +0100

    Titanium Update

--- src/main/java/BoosterPacks/cards/red/Titanium.java
+++ src/main/java/BoosterPacks/cards/red/Titanium.java
@@ -1,9 +1,8 @@
 package BoosterPacks.cards.red;
 
 import BoosterPacks.BoosterPacks;
-import BoosterPacks.powers.ironclad.TitaniumPower;
 import basemod.abstracts.CustomCard;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
@@ -29,20 +28,24 @@ public class Titanium extends CustomCard {
 
     public Titanium() {
         super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
-        this.exhaust = true;
+        this.baseBlock = 5;
+        this.block = this.baseBlock;
+        this.baseMagicNumber = 2;
+        this.magicNumber = this.baseMagicNumber;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new ApplyPowerAction(p, p, new TitaniumPower(p, 1), 1));
+        for (int i = 0; i < this.magicNumber; i++) {
+            this.addToBot(new GainBlockAction(p, p, this.block));
+        }
     }
 
     @Override
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
-            this.exhaust = false;
-            this.rawDescription = cardStrings.UPGRADE_DESCRIPTION;
+            this.upgradeMagicNumber(1);
             initializeDescription();
         }
     }
